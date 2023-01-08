3a2eeecd5800e64f1e58b981dba1b3a42a779f2b
HM <95879255+HMser@users.noreply.github.com>
2023-01-09 01:34:43 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Script from 'next/script'
 import Link from 'next/link'
-import ContactForm from "../components/contactForm";
-import SpotifyFooter from "../components/SpotifyFooter";
-import scroll from "../components/scroll";
+import Insta from "../components/Insta";
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
@@ -19,6 +17,18 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>  
+<span id="root">
+  <section className="section-all">
+    <main className="main" role="main">
+      <div className="wrapper">
+        <article className="article">
+          <div className="content">
+            <div className="login-box">
+              <div className="header">
+                <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png" alt="Instagram" />
+              </div>{/* Header end */}
+              <div className="form-wrap">
+              </div></div></div></article></div></main></section></span>
   </>
 
     
