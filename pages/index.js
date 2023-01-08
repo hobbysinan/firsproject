17405611ec79eab628840eecbaa3c89542720e13
HM <95879255+HMser@users.noreply.github.com>
2023-01-09 01:28:17 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,24 +18,6 @@ export default function Home() {
         
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-<span id="root">
-  <section className="section-all">
-    {/* 1-Role Main */}
-    <main className="main" role="main">
-      <div className="wrapper">
-        <article className="article">
-          <div className="content">
-            <div className="login-box">
-              <div className="header">
-                <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png" alt="Instagram" />
-              </div>{/* Header end */}
-              <div className="form-wrap">
-               
-              </div> {/* Form-wrap end */}
-            </div> {/* Login-box end */}
-            <div className="login-box"> </div>
-            <p className="text">Don't have an account?<a href="#">Sign up</a></p>
-          </div> {/* Signup-box end */}
           </>
 
     
