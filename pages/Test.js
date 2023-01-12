e7586c06242608f94cc14dfc773a7bc4fd207518
HM <95879255+HMser@users.noreply.github.com>
2023-01-12 23:24:21 +0530

    Update Test.js

--- pages/Test.js
+++ pages/Test.js
@@ -1,8 +1,8 @@
 import styles from '../styles/Home.module.css'
 export default function FirstPost() {
-   return <h1>My First Post</h1>
-
- <h3> Improved Labels </h3> 
+   return 
+<h1>Improved Captions</h1>
+ 
 <p>After hashtags, captions are the most important cog in an Instagram post. Your captions need to be engaging, creative and should prompt the followers for a response. This can result in a lot of comments which is great for engagement numbers.</p> 
 
 }
