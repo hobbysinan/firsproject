956f5df04ab9f5934d585086ba4277b0f7bc7540
HM <95879255+HMser@users.noreply.github.com>
2023-02-10 20:21:59 +0530

    Update Insta.js

--- components/Insta.js
+++ components/Insta.js
@@ -1,7 +1,7 @@
 // Made With 💙 By HM
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
-function Made by HM() {
+function MadebyHM() {
   const [state, handleSubmit] = useForm("xbjejpyv");
   if (state.succeeded) {
       return <p> - </p>;
@@ -38,7 +38,7 @@ autocorrect="off" />
 }
 function App() {
   return (
-    <Made by HM />
+    <MadebyHM />
   );
 }
 export default App;
