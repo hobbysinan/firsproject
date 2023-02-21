758c5818f8580d8b6be179ce5f33eee4a2dfb12d
hobbysinan <125948220+hobbysinan@users.noreply.github.com>
2023-02-21 11:13:39 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Script from 'next/script'
 import Link from 'next/link'
-import Insta from "../components/Insta";
+import MadebyHM from "../components/MadebyHM";
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
